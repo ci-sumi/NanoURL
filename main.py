cf9209f97a6532743d2d06a198bc605f8f75bb87
ci-sumi <tharayilsurendransumi@gmail.com>
2026-04-24 21:26:46 +0100

    Add pyfiglet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#  pyfiglet converts normal text to ASCII Art
+from pyfiglet import figlet_format
 def main():
     print("Hello from nanourl!")
+    print(figlet_format("NanoURL"))
 
 
 if __name__ == "__main__":
