12a523bf8e32a6a5d13b9d11813bf26a52d642be
Tomislav Dukez <tomdu3@ymail.com>
2026-04-28 15:38:40 +0100

    add test_color script

---
+++ test_color.py
@@ -0,0 +1,9 @@
+# Cyan color ANSI escape code
+cyan = "\033[96m"
+red = "\033[91m"
+blue = "\033[94m"
+yellow = "\033[33m"
+# Reset color
+reset = "\033[0m"
+
+print(f"This text is normal. {cyan}This text will be cyan. {red}This text will be red. {blue}This text will be blue. {yellow}This text will be yellow.{reset}")
