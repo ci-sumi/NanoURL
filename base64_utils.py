a0ffbf37e5b8f5533f15abffe916e2be6e679869
ci-sumi <tharayilsurendransumi@gmail.com>
2026-04-28 08:47:22 +0100

    Generate a  6-letter short code using string and  random

--- base64_utils.py
+++ base64_utils.py
@@ -1,8 +1,19 @@
+
 from flask import Flask,redirect
 import base64
+#Predefined sets of characters
+import string
+import random
+# To Store the short URL and their associated URLS
+url_map={}
 nanourl=Flask(__name__)
 @nanourl.route('/')
-def home():
-    return "Hello nanoUrl" 
+# def home():
+#     return "Hello nanoUrl" 
+def generate_short_url():
+    characters = string.ascii_uppercase+string.ascii_lowercase
+    return "".join(random.choices(characters,k=6))
+    
+    
 if __name__=='__main__':
     nanourl.run(debug=True)
