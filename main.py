73c2cfb57dc8b4bd611cea3bfe3d0511773b4cbe
Tomislav Dukez <tomdu3@ymail.com>
2026-05-01 09:58:54 +0100

    fix imports and add __init__.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import base64
 #Predefined sets of characters
 import string
 import random
+from tools.url_shortner import shorten_url_pyshorteners
 # To Store the short URL and their associated URLS
 url_map={}
-nanourl=Flask(__name__,template_folder="../templates")
+nanourl=Flask(__name__)
  
 def generate_short_url():
     characters = string.ascii_uppercase+string.ascii_lowercase
@@ -14,9 +15,10 @@ def generate_short_url():
     
 @nanourl.route('/',methods=["GET","POST"])
 def index():
+    google_sh = shorten_url_pyshorteners("google.com")
     shorten_url=None
     if request.method=="POST":
         shorten_url=generate_short_url()
-    return render_template("index.html",shorten_url=shorten_url)
+    return render_template("index.html",shorten_url=google_sh)
 if __name__=='__main__':
     nanourl.run(debug=True)
