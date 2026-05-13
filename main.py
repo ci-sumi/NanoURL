902817f018ffb2b3ea3729d6bfb30b9121ccf4e7
ci-sumi <tharayilsurendransumi@gmail.com>
2026-05-13 20:07:15 +0100

    Set up SQlAlchemy

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 
 from flask import Flask,redirect,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 import base64
 #Predefined sets of characters
 import string
 import random
 from tools.url_shortner import shorten_url_pyshorteners
+
 # To Store the short URL and their associated URLS
 url_map={}
 nanourl=Flask(__name__)
+#Configuring the database
+nanourl.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+nanourl.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#Creating the database instance
+db=SQLAlchemy(nanourl)
+
+@nanourl.route("/c")
+def home():
+    return "Database connected"
+
+class User(db.Model):
+    id =db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"id :{self.id}name:{self.name}"
+    
+
  
 def generate_short_url():
     characters = string.ascii_uppercase+string.ascii_lowercase
@@ -20,5 +40,9 @@ def index():
     if request.method=="POST":
         shorten_url=generate_short_url()
     return render_template("index.html",shorten_url=google_sh)
+
+# Run the application
 if __name__=='__main__':
+    with nanourl.app_context():
+        db.create_all()
     nanourl.run(debug=True)
